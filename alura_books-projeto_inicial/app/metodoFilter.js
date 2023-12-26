b2433aefce55be011df4cdefced3cfa01388dfde
Vinicius Pedro De Souza <125681836+vinips1993@users.noreply.github.com>
2023-12-26 16:56:34 -0300

    Add files via upload
    Upload Project

---
+++ alura_books-projeto_inicial/app/metodoFilter.js
@@ -0,0 +1,33 @@
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = categoria == 'disponivel' ?
+    filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis
+        (livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisnaTela(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria
+        == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisnaTela(valorTotal) { 
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}
+    </span></p>
+  </div>
+    `
+}
